5a6558694310171b219cf268eb9245911a87d4c5
Sid <sidfarronantoine@gmail.com>
2024-06-17 16:49:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ document.addEventListener("DOMContentLoaded", function() {
     aboutLink.style.color = "#d4b15c";
 
     //assign links functionality
-    aboutLink.addEventListener("click", clickAbout);
-    experienceLink.addEventListener("click", clickExperience);
-    projectsLink.addEventListener("click", clickProjects);
-    skillsLink.addEventListener("click", clickSkills);
+    aboutLink.addEventListener("click", clickAbout, false);
+    experienceLink.addEventListener("click", clickExperience, false);
+    projectsLink.addEventListener("click", clickProjects, false);
+    skillsLink.addEventListener("click", clickSkills, false);
 });
 
-function clickAbout() {
+function clickAbout(event) {
     //display the about section only
     about.style.display = "block";
     about.classList.add("fade-in-text");
@@ -41,9 +41,12 @@ function clickAbout() {
 
     //style links accordingly
     linkStyler("about");
+
+    //prevent default scrollling behavior
+    event.preventDefault();
 }
 
-function clickExperience() {
+function clickExperience(event) {
     //display the experience section only  
     experience.style.display = "block";
     experience.classList.add("fade-in-text");
@@ -54,9 +57,12 @@ function clickExperience() {
 
     //style links accordingly
     linkStyler("experience");
+
+    //prevent default scrollling behavior
+    event.preventDefault();
 }
 
-function clickProjects() {
+function clickProjects(event) {
     //display the projects section only
     projects.style.display = "block";
     projects.classList.add("fade-in-text");
@@ -67,6 +73,9 @@ function clickProjects() {
 
     //style links accordingly
     linkStyler("projects");
+
+    //prevent default scrollling behavior
+    event.preventDefault();
 }
 
 function clickSkills(){
@@ -80,6 +89,9 @@ function clickSkills(){
 
     //style links accordingly
     linkStyler("skills");
+
+    //prevent default scrollling behavior
+    event.preventDefault();
 }
 
 function linkStyler(section) {
