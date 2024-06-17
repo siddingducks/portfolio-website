d80bb2ddae40a53baed154d1205c48420bec7ac1
Sid <sidfarronantoine@gmail.com>
2024-06-17 12:19:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,104 @@
-//home page
-window.onload = function() {
-    //set visibility
-    document.querySelector("#about").style.display = "block";
-    document.querySelector("#experience").style.display = "none";
-    document.querySelector("#projects").style.display = "none";
-    document.querySelector("#skills").style.display = "none";
+//create variables
+let about, experience, projects, skills;
+let aboutLink, experienceLink, projectsLink, skillsLink;
 
-    //assign buttons functionality
-    document.querySelector("#about-link").addEventListener("click", about);
-    document.querySelector("#experience-link").addEventListener("click", experience);
-    document.querySelector("#projects-link").addEventListener("click", projects);
-    document.querySelector("#skills-link").addEventListener("click", skills);
-};
+document.addEventListener("DOMContentLoaded", function() {
+    //assign values variables
+    about = document.getElementById("about");
+    experience = document.getElementById("experience");
+    projects = document.getElementById("projects");
+    skills = document.getElementById("skills");
 
-function about() {
-    document.querySelector("#about").style.display = "block";
-    document.querySelector("#experience").style.display = "none";
-    document.querySelector("#projects").style.display = "none";
-    document.querySelector("#skills").style.display = "none";
+    aboutLink = document.getElementById("about-link");
+    experienceLink = document.getElementById("experience-link");
+    projectsLink = document.getElementById("projects-link");
+    skillsLink = document.getElementById("skills-link");
+
+    //set visibility for home page
+    about.style.display = "block";
+    experience.style.display = "none";
+    projects.style.display = "none";
+    skills.style.display = "none";
+
+    //style landing page link
+    aboutLink.style.color = "#d4b15c";
+
+    //assign links functionality
+    aboutLink.addEventListener("click", clickAbout);
+    experienceLink.addEventListener("click", clickExperience);
+    projectsLink.addEventListener("click", clickProjects);
+    skillsLink.addEventListener("click", clickSkills);
+});
+
+function clickAbout() {
+    //display the about section only
+    about.style.display = "block";
+    about.classList.add("fade-in-text");
+
+    experience.style.display = "none";
+    projects.style.display = "none";
+    skills.style.display = "none";
+
+    //style links accordingly
+    linkStyler("about");
 }
 
-function experience() {
-    document.querySelector("#about").style.display = "none";
-    document.querySelector("#experience").style.display = "block";
-    document.querySelector("#projects").style.display = "none";
-    document.querySelector("#skills").style.display = "none";
+function clickExperience() {
+    //display the experience section only  
+    experience.style.display = "block";
+    experience.classList.add("fade-in-text");
+
+    about.style.display = "none";
+    projects.style.display = "none";
+    skills.style.display = "none";
+
+    //style links accordingly
+    linkStyler("experience");
 }
 
-function projects() {
-    document.querySelector("#about").style.display = "none";
-    document.querySelector("#experience").style.display = "none";
-    document.querySelector("#projects").style.display = "block";
-    document.querySelector("#skills").style.display = "none";
+function clickProjects() {
+    //display the projects section only
+    projects.style.display = "block";
+    projects.classList.add("fade-in-text");
+
+    about.style.display = "none";
+    experience.style.display = "none";
+    skills.style.display = "none";
+
+    //style links accordingly
+    linkStyler("projects");
 }
 
-function skills(){
-    document.querySelector("#about").style.display = "none";
-    document.querySelector("#experience").style.display = "none";
-    document.querySelector("#projects").style.display = "none";
-    document.querySelector("#skills").style.display = "block";
+function clickSkills(){
+    //display the skills section only  
+    skills.style.display = "block";
+    skills.classList.add("fade-in-text");
+    
+    about.style.display = "none";
+    experience.style.display = "none";
+    projects.style.display = "none";
+
+    //style links accordingly
+    linkStyler("skills");
+}
+
+function linkStyler(section) {
+    aboutLink.setAttribute('style', 'color: #FFFFFF !important');
+    experienceLink.setAttribute('style', 'color: #FFFFFF !important');
+    projectsLink.setAttribute('style', 'color: #FFFFFF !important');
+    skillsLink.setAttribute('style', 'color: #FFFFFF !important');
+
+    if (section == "about") {
+        aboutLink.style.color = "#d4b15c";
+    }
+    else if (section == "experience") {
+        experienceLink.style.color = "#d4b15c";
+    }
+    else if (section == "projects") {
+        projectsLink.style.color = "#d4b15c";
+    }
+    else {
+        //if section == "skills"
+        skillsLink.style.color = "#d4b15c";
+    }
 }
