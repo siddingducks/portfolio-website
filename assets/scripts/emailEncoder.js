34c7af3a4f265effde95e08e5a24073a8877a6c2
Sid <sidfarronantoine@gmail.com>
2024-06-20 11:44:37 -0400

    Create emailEncoder.js

---
+++ assets/scripts/emailEncoder.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll("[data-part1][data-part2][data-part3]");
+for (const link of links) {
+  const attrs = link.dataset;
+  link.setAttribute(
+    "href",
+    `mailto:${attrs.part1}@${attrs.part2}.${attrs.part3}?subject=${attrs.subject}`
+  );
+  link.textContent = `${attrs.part1}@${attrs.part2}.${attrs.part3}`;
+}
